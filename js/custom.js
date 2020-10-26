2998b77c47124d6a3bba17ef8879874f7df39db5
AsmaaEbeed <eng.asmaa_ebeed@yahoo.com>
2020-10-26 12:43:26 +0200

    website

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,11 @@
 
     // PRE LOADER
     $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
+      $('.preloader').fadeOut(1000); // set duration in brackets  
+      //$(".vid").css("width", "50%");
+      document.getElementById('vid').play();
+
+      
     });
 
 
@@ -50,7 +54,42 @@
     
    $('.slider .circle-image') .load(function(){
        $(this).animate({top: "100px"});
-   })
+
+   });
+
+       /*pop-up*/
+       $('#show_popup').click(function () {
+        //$($(this).data('popup')).fadeIn();
+        //alert("kk");
+        $('.popup').css("display", "block");
+        //document.body.style.position = 'fixed';
+        //document.body.style.top = `-${window.scrollY}px`;
+        //$('.popup').style.overflow = 'auto';
+    });
+    $('.popup').click(function () {
+        $(this).fadeOut();
+        
+    });
+    $('.popup .inner-popup').click(function (e) {
+        e.stopPropagation();    //prevent inner-pop close when click on it
+    });
+    $('.popup .close-popup').click(function () {
+        //e.preventDefault();
+        //$(this).parentsUntil('.popup').parent().fadeOut();
+        //$('.popup').css("display", "none");
+        $('.popup').fadeOut();
+    });
+    $('.popup .redirect-to-page').click(function(){
+      $('.popup').fadeOut();
+      window.location.href = "https://en.wikipedia.org/wiki/Policy";
+    });
+    //when press esc btn in keyboard close popup
+    $(document).keydown(function() {
+        if (e.keyCode == 27) {
+            $('.popup').fadeOut();
+            
+        }
+    });
 
 })(jQuery);
 
